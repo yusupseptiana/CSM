82244639a6923425e1983ed618b7201c99052100
yusupseptiana12 <yusupseptiana12@gmail.com>
2026-06-05 22:49:00 +0700

    Add environment variables

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,12 @@
+require("dotenv").config();
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "training_db"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 db.connect(err => {
